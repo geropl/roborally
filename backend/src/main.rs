736384e2a0b52f6f71a05c3736398887b20b7fcc
Gero Posmyk-Leinemann <gero.posmyk-leinemann@typefox.io>
2019-10-30 10:24:33 +0000

    Simple protocol

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+
+mod protocol;
+use protocol::{
+    server::{Greeter, GreeterServer},
+    HelloReply, HelloRequest,
+};
+
+#[derive(Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello( &self, request: Request<HelloRequest>) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = protocol::HelloReply {
+            message: format!("Hello {}!", request.into_inner().name).into(),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:50051".parse().unwrap();
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .serve(addr, GreeterServer::new(greeter))
+        .await?;
+
+    Ok(())
 }
